3e551c1c5aa7f00f59cdda39ddcc405389bb6c51
unknown <prasiddha.kansakar@gmail.com>
2022-04-25 10:55:37 +0545

    searchBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {React,useState} from "react";
+import Input from "./Components/Input";
+import TextField from "@mui/material/TextField";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+    const [inputText, setInputText] = useState("");
+
+    let inputHandler = (e) => {
+        var lowerCase = e.target.value.toLowerCase();
+        setInputText(lowerCase);
+    }
+
+    return (
+    <div>
+        <TextField
+          onChange={inputHandler}
+          id="standard-search"
+          label="Search field"
+          type="search"
+          variant="standard"/>
+        <Input input={inputText}/>
     </div>
-  );
+    )
 }
+
 
 export default App;
